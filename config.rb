4bf80d410891d85fbf894874cd1923eea1cd2b3c
ymat2 <yuki.matsuda.r7@dc.tohoku.ac.jp>
2023-05-28 13:28:43 +0900

    :sparkles: Initial commit

---
+++ config.rb
@@ -0,0 +1,32 @@
+require 'gollum/app'
+
+
+## General options
+wiki_options = {
+  page_file_dir: 'source',
+  css: true,
+  mathjax: false,
+  emoji: true
+}
+Precious::App.set(:wiki_options, wiki_options)
+
+
+## Basic comfirmation
+module Precious
+  class App < Sinatra::Base
+    use Rack::Auth::Basic, 'Private Wiki' do |username, password|
+      users = File.open(File.expand_path('users.json', __dir__)) do |file|
+        JSON.parse(file.read, symbolize_names: true)
+      end
+      name = username.to_sym
+      digested = Digest::SHA256.hexdigest(password)
+      if users.key?(name) && digested == users[name][:password]
+        Precious::App.set(:author, users[name])
+      end
+    end
+
+    before do
+      session['gollum.author'] = settings.author
+    end
+  end
+end
